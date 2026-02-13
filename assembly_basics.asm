8c4358b57e117e3a948fbbd82544c822ab80ff6a
Oskar Norin <oskar.norin93@gmail.com>
2026-02-13 15:09:07 +0100

    inital commit lab 3

---
+++ assembly_basics.asm
@@ -0,0 +1,13 @@
+Lab 3 digsys
+
+1.1 Write an assembly program that load the values 3 and 4 into r16 an r17
+add the result to r17
+
+ldi r16, 3
+ldi r17, 4
+add r17, r16
+
+1.2 Assume a user have entered their age in a program. The value is stored in register r16. Write a
+program such that if the age is above or equal to 18, you store the value 1 in register r24, otherwise
+you store the value 2 in r24. After the check, you shall just enter an infinite loop.
+
